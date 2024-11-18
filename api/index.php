19ec35476cd9815b9b76adbd35751b165268b8c6
Davidson1004 <veghdavidpatrik@gmail.com>
2024-11-18 13:05:48 +0100

    1 lekérés kész, második félig kidolgozva

--- api/index.php
+++ api/index.php
@@ -5,11 +5,13 @@ include "./adatbazisFuggveny.php";
 $url = explode("/", $_SERVER["REQUEST_URI"]);
 switch (end($url)) {
     case 'stat_yearly_income':
-        #missing year grouping
-        $response = lekeres("SELECT places.name, AVG(orders.price) as 'average_spending' FROM `orders` INNER JOIN places ON orders.place_id = places.id GROUP BY orders.place_id;", "bufego");
+        $body = file_get_contents("php://input");
+        $body = json_decode($body, true);
+        $response = lekeres("SELECT places.name, YEAR(orders.collected_at) as 'ev', MONTH(orders.collected_at) as 'honap', AVG(orders.price) as 'average_spending' FROM `orders` INNER JOIN places ON orders.place_id = places.id GROUP BY YEAR(orders.collected_at), MONTH(orders.collected_at) HAVING name='".$body['bufe_name']."';", "bufego");
         echo json_encode($response, JSON_UNESCAPED_UNICODE);
         break;
-    
+    case 'legjobbanfogyo':
+
     default:
         echo json_encode(['valasz' => 'Hibás url'], JSON_UNESCAPED_UNICODE);
         break;
