9fca2b23385e6423f0f351dcabab7e3793feff32
Farever <nagylajosdominik@gmail.com>
2024-11-25 12:47:55 +0100

    felhasznlói adatok modósítása, lekérése, felhasználó regisztráció sql kérések

--- bufego/php/api.php
+++ bufego/php/api.php
@@ -1,18 +1,42 @@
 <?php
-    //PHP telepítés kell!!!
-    //php -S localhost:8000 command a php mappában
+header('Access-Control-Allow-Origin: http://localhost:3000');
 
-    header('Access-Control-Allow-Origin: http://localhost:3000');
-    include './db.php';
+$url = explode('/', $_SERVER["PHP_SELF"]);
+$endpoint = explode('?', end($url))[0];
 
-    $url = explode('/',$_SERVER["PHP_SELF"]);
+include './db.php';
+include './felhasznalok.php';
 
-    switch(end($url)){
-        case 'test':
-            echo "Test";
-            break;
-        default:
-            break;
-    }
+switch ($endpoint) {
+    case 'felhasznaloadatok':
+            if ($_SERVER["REQUEST_METHOD"] == "GET") {
+                if (!empty($_GET['userId'])) {
+                    echo json_encode(felhasznaloiAdatokLekerese($_GET['userId']), JSON_UNESCAPED_UNICODE);
+                } else {
+                    header('bad request', true, 400);
+                    echo json_encode(['valasz' => 'Hiányzó adatok!'], JSON_UNESCAPED_UNICODE);
+                }
+            } else {
+                header('bad request', true, 400);
+                echo json_encode(['valasz' => 'Hibás metődus!'], JSON_UNESCAPED_UNICODE);
+            }
+        break;
 
-?>
+    case  'felhasznaloregisztracio':
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (!empty($_POST['email']) && !empty($_POST['passcode']) && !empty($_POST['name']) && !empty($_POST['address_id']) && !empty($_POST['phone']) && !empty($_POST['school'])) {
+                $adatok = felhasznaloAdatokFeltoltese($_POST['email'], $_POST['passcode'], $_POST['name'], $_POST['address_id'], $_POST['phone'], $_POST['school'], NULL, 0);
+                echo json_encode($adatok, JSON_UNESCAPED_UNICODE);
+            } else {
+                header('bad request', true, 400);
+                echo json_encode(['valasz' => 'Hiányzó adatok!'], JSON_UNESCAPED_UNICODE);
+            }
+        } else {
+            header('bad request', true, 400);
+            echo json_encode(['valasz' => 'Hibás metődus!'], JSON_UNESCAPED_UNICODE);
+        }
+        break;
+
+    default:
+        break;
+}
--- bufego/php/db.php
+++ bufego/php/db.php
@@ -1,7 +1,7 @@
 <?php
     function lekeres($query, $database)
     {
-        $db=new mysqli("localhost", "root", "", $database);
+        $db= new mysqli("localhost", "root", "", $database);
         if($db->connect_errno == 0)
         {
             $response = $db->query($query);
@@ -28,17 +28,18 @@
         }
     }
 
-    function valtoztatas($query, $database)
+    function valtoztatas($queryIn, $database)
     {
-        $db=new mysqli("localhost", "root", "", $database);
+        $db= new mysqli("localhost", "root", "", $database);
         if($db->connect_errno == 0)
         {
-            $response = $db->query($query);
+            $db -> query("SET FOREIGN_KEY_CHECKS=0;");
+            $db->query($queryIn);
             if($db->errno == 0)
             {
                 if($db->affected_rows > 0)
                 {
-                    return "Sikeres módosítás";
+                    return "Sikeres módosítás!";
                 }
                 else if($db->affected_rows == 0)
                 {
@@ -53,6 +54,40 @@
             {
                 return $db->error;
             }
+            $db -> query("SET FOREIGN_KEY_CHECKS=1;");
+        }
+        else
+        {
+            return $db->connect_error;
+        }
+    }
+
+    function beszuras($query, $database){
+        $db= new mysqli("localhost", "root", "", $database);
+        if($db->connect_errno == 0)
+        {
+            $db -> query("SET FOREIGN_KEY_CHECKS=0;");
+            $db->query($query);
+            if($db->errno == 0)
+            {
+                if($db->affected_rows > 0)
+                {
+                    return $db->insert_id;
+                }
+                else if($db->affected_rows == 0)
+                {
+                    return "Sikertelen müvelet!";
+                }
+                else
+                {
+                    return $db->error;
+                }
+            }
+            else
+            {
+                return $db->error;
+            }
+            $db -> query("SET FOREIGN_KEY_CHECKS=1;");
         }
         else
         {
