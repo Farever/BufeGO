b60865d49eca39cc13f6f06ee7e22a46334f294d
Davidson1004 <veghdavidpatrik@gmail.com>
2025-03-01 17:13:35 +0100

    Termékfelvétel hozzáadva

--- bufego/src/components/ProductUploadForm.jsx
+++ bufego/src/components/ProductUploadForm.jsx
@@ -1,5 +1,5 @@
-import { Form, Modal, Button } from "react-bootstrap"
-import { useState, useEffect } from "react";
+import { Form, Modal, Button, Alert } from "react-bootstrap"
+import { useState, useEffect, useRef } from "react";
 import axios from 'axios';
 import ActionButton from "./ActionButton";
 import "../styles/UploadModal.css"
@@ -9,6 +9,10 @@ const ProductUploadForm = () => {
     const [categories, setCategories] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    const [uploadstatus, setUploadStatus] = useState("");
+    const [responseMessage, setResponseMessage] = useState("");
+
     const refreshInterval = 5000; // Alapértelmezett frissítési idő 5 másodperc
 
     const fetchCategories = async () => {
@@ -39,27 +43,46 @@ const ProductUploadForm = () => {
       }, [refreshInterval]);
 
       const uploadProduct = async() => {
+        console.log(category.current.value)
         let response = await fetch('http://localhost/13c-vegh/api/index.php/termek_felv', {
             headers: {
-                Content_Type: "application/json"
+                "Content-Type": "application/json"
             },
             method: "POST",
             body: JSON.stringify({
-                place: "",
-                category: "",
-                img: "",
-                name: "",
-                description: "",
-                allergens: "",
-                is_available: "",
-                price: ""
+                place: place_id,
+                category: category.current.value,
+                img: img.current.value,
+                name: product_name.current.value,
+                description: product_desc.current.value,
+                allergens: allergens.current.value,
+                is_avaliable: availability.current.checked,
+                price: parseFloat(price.current.value)
             })
         })
-        console.log(response);
-      }
+        let data = await response.json();
+        if(response.ok)
+        {
+            setUploadStatus("success")
+            setResponseMessage("Sikeres adatfelvétel");
+        }
+        else
+        {
+            setUploadStatus("danger");
+            setResponseMessage(data.valasz);
+        }
+        
+    }
 
 
-    const place_id = 1;
+    let place_id = 1;
+    const product_name = useRef("");
+    const category = useRef("");
+    const img = useRef("");
+    const product_desc = useRef("");
+    const allergens = useRef("");
+    const availability = useRef(true);
+    const price = useRef(0);
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -77,42 +100,46 @@ const ProductUploadForm = () => {
                     <Form>
                         <Form.Group className="mb-3">
                             <Form.Label>Termék neve</Form.Label>
-                            <Form.Control type="text" />
+                            <Form.Control type="text" ref={product_name}/>
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Kategória</Form.Label>
-                            <Form.Select>
+                            <Form.Select ref={category}>
                             {
                                 categories.map((category)=>{
-                                    return <option key={category.id} value={category.categroy_name}>{category.categroy_name}</option>
+                                    return <option key={category.id} value={category.id}>{category.categroy_name}</option>
                                 })
                             }
                             </Form.Select>
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Kép placeholder</Form.Label> {/*TODO Kép feltöltését megoldani */}
-                            <Form.Control type="text" />
+                            <Form.Control type="text" ref={img}/>
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Description</Form.Label>
-                            <Form.Control type="text" />
+                            <Form.Control type="text" ref={product_desc}/>
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Alergens</Form.Label>
-                            <Form.Control type="text" />
+                            <Form.Control type="text" ref={allergens}/>
                         </Form.Group>
                         <Form.Group className="mb-3">
-                            <Form.Check type="checkbox" label="Available" className="checkbox"></Form.Check>
+                            <Form.Check type="checkbox" label="Available" className="checkbox" ref={availability}></Form.Check>
                         </Form.Group>
                         <Form.Group className="mb-3">
                             <Form.Label>Price</Form.Label> {/*TODO Kép feltöltését megoldani */}
-                            <Form.Control type="number" />
+                            <Form.Control type="number" ref={price}/>
                         </Form.Group>
                     </Form>
+                    <Alert variant={uploadstatus}>{responseMessage}</Alert>
                 </Modal.Body>
                 <Modal.Footer>
+                
                 <ActionButton type="reject" onClick={handleClose}></ActionButton>
-                <ActionButton type="accept" onClick={handleClose}></ActionButton>
+                <ActionButton type="accept" onClick={() => {
+                    uploadProduct();
+                }}></ActionButton>
                 </Modal.Footer>
             </Modal>
         </>
