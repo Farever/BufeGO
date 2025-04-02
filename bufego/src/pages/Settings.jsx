b306bba6661101169a89b87419064fe555cd18c7
Farever <nagylajosdominik@gmail.com>
2025-04-02 11:37:30 +0200

    build done

--- bufego/src/pages/Settings.jsx
+++ bufego/src/pages/Settings.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, useContext } from "react";
-import { useEffect, useState, useContext } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { AuthContext } from '../Contexts';
