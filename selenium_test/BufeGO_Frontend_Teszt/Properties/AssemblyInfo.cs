142861eb3954c03d4a553a4845dcc89b548c00bd
Davidson1004 <veghdavidpatrik@gmail.com>
2025-04-15 21:44:02 +0200

    Visszavonás egy hibás feltöltésből

---
+++ selenium_test/BufeGO_Frontend_Teszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BufeGO_Frontend_Teszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BufeGO_Frontend_Teszt")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29a8ea86-8c9d-477e-980b-90782feb7977")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
